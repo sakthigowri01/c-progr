bbee51c8e42b95b13e9cbe1d23353401626bb450
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-12 13:34:33 +0530

    c.c

---
+++ c.c
@@ -0,0 +1,11 @@
+int main()
+{
+    int arr[] = {2, 5, 2, 6, -1, 9999999, 5, 8, 8, 8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    sortByFrequency(arr, n);
+ 
+    for (int i=0; i<n; i++)
+      
+    return 0;
+}
